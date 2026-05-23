f6b9f5a36d1c1bd6f25806e1ad63cffda0ebec21
Alexander <alexby2g@gmail.com>
2026-05-23 09:35:10 -0400

    update glamur layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -28,16 +28,66 @@
       <q-list>
 
         <q-item-label header>
-          Menú
+          Menú Principal
         </q-item-label>
 
-        <q-item clickable to="/">
+        <q-item clickable to="/dashboard">
           <q-item-section avatar>
-            <q-icon name="home" />
+            <q-icon name="dashboard" />
           </q-item-section>
 
           <q-item-section>
-            Inicio
+            Dashboard
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/clientes">
+          <q-item-section avatar>
+            <q-icon name="people" />
+          </q-item-section>
+
+          <q-item-section>
+            Clientes
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/citas">
+          <q-item-section avatar>
+            <q-icon name="event" />
+          </q-item-section>
+
+          <q-item-section>
+            Citas
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/calendario">
+          <q-item-section avatar>
+            <q-icon name="calendar_month" />
+          </q-item-section>
+
+          <q-item-section>
+            Calendario
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/pagos">
+          <q-item-section avatar>
+            <q-icon name="payments" />
+          </q-item-section>
+
+          <q-item-section>
+            Pagos
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable to="/historial">
+          <q-item-section avatar>
+            <q-icon name="history" />
+          </q-item-section>
+
+          <q-item-section>
+            Historial
           </q-item-section>
         </q-item>
 
