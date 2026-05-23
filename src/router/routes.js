0a5586714d9bc4e1f3ced1962640d55592303a64
Alexander <alexby2g@gmail.com>
2026-05-23 09:38:15 -0400

    add historial calendario routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,48 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
+
     children: [
-      { path: '', redirect: '/dashboard' },
-      { path: 'dashboard', name: 'dashboard', component: () => import('pages/DashboardPage.vue') },
-      { path: 'clientes', name: 'clientes', component: () => import('pages/ClientesPage.vue') },
-      { path: 'citas', name: 'citas', component: () => import('pages/CitasPage.vue') },
-      { path: 'pagos', name: 'pagos', component: () => import('pages/PagosPage.vue') }
+      {
+        path: '',
+        redirect: '/dashboard'
+      },
+
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => import('pages/DashboardPage.vue')
+      },
+
+      {
+        path: 'clientes',
+        name: 'clientes',
+        component: () => import('pages/ClientesPage.vue')
+      },
+
+      {
+        path: 'citas',
+        name: 'citas',
+        component: () => import('pages/CitasPage.vue')
+      },
+
+      {
+        path: 'pagos',
+        name: 'pagos',
+        component: () => import('pages/PagosPage.vue')
+      },
+
+      {
+        path: 'calendario',
+        name: 'calendario',
+        component: () => import('pages/CalendarioPage.vue')
+      },
+
+      {
+        path: 'historial',
+        name: 'historial',
+        component: () => import('pages/HistorialPage.vue')
+      }
     ]
   },
 
