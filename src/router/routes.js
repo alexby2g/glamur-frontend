37e9d595d9c1ed7ad68bc8b0db6fb8414d375e6a
Alexander <alexby2g@gmail.com>
2026-05-23 09:23:44 -0400

    fix routes glamur

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,30 +3,11 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      {
-        path: '',
-        redirect: '/dashboard'
-      },
-      {
-        path: 'dashboard',
-        name: 'dashboard',
-        component: () => import('pages/DashboardPage.vue')
-      },
-      {
-        path: 'clientes',
-        name: 'clientes',
-        component: () => import('pages/ClientesPage.vue')
-      },
-      {
-        path: 'citas',
-        name: 'citas',
-        component: () => import('pages/CitasPage.vue')
-      },
-      {
-        path: 'pagos',
-        name: 'pagos',
-        component: () => import('pages/PagosPage.vue')
-      }
+      { path: '', redirect: '/dashboard' },
+      { path: 'dashboard', name: 'dashboard', component: () => import('pages/DashboardPage.vue') },
+      { path: 'clientes', name: 'clientes', component: () => import('pages/ClientesPage.vue') },
+      { path: 'citas', name: 'citas', component: () => import('pages/CitasPage.vue') },
+      { path: 'pagos', name: 'pagos', component: () => import('pages/PagosPage.vue') }
     ]
   },
 
